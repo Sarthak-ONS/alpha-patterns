6eb250c0311064a6ee6160f382a134cd09805117
Sarthak Agarwal <agarwalsarthak456@gmail.com>
2022-12-25 20:05:47 +0530

    Completed U and refractored code

--- U.java
+++ U.java
@@ -2,6 +2,11 @@ public class U {
 
   public static void main(String[] args) {
     int n = 8;
+    print_alphabet_U(n);
+  }
+
+  // Below Function prints U
+  static void print_alphabet_U(int n) {
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
         if (j == n - 1 || j == 0) System.out.print(
